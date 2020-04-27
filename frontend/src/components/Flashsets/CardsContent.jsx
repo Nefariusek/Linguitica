7e31d4968152a9b2d9bf93484cd3f25ac18a9d1e
Piotrek <piotrekste98@gmail.com>
2020-04-27 19:13:45 +0200

    changed style of flashcard

--- frontend/src/components/Flashsets/CardsContent.jsx
+++ frontend/src/components/Flashsets/CardsContent.jsx
@@ -1,55 +1,53 @@
 import React, { Component } from 'react';
-import { Card, Button, Pagination, Row, Modal, Input } from 'antd';
-import { EditOutlined } from '@ant-design/icons';
+import { List, Modal, Input, Button } from 'antd';
+import { EditOutlined, StarOutlined, StarFilled } from '@ant-design/icons';
+import setHeaders from '../../utils/setHeaders';
 class CardsContent extends Component {
   state = {
     minValue: 0,
     maxValue: 10,
     open: false,
     curID: 0,
-    curTitle: '',
-    curValue: '',
+    temp: false,
+    curPolish: '',
+    curGerman: '',
+
     results: [],
-    data: [
-      { id: 0, title: 'Fiszka 1', polish: 'monitor', german: 'der Bildschrim', category: 'elektronika' },
-      { id: 1, title: 'Fiszka 2', polish: 'samochód', german: 'das Auto', category: 'pojazdy' },
-      { id: 2, title: 'Fiszka 3', polish: 'syn', german: 'der Sohn', category: 'ludzie' },
-      { id: 3, title: 'Fiszka 4', polish: 'miotła', german: 'der Besen', category: 'dom' },
-      {
-        id: 4,
-        title: 'Fiszka 5',
-        polish: 'żołnierz',
-        german: 'der Kragen',
-        category: 'ludzie',
-      },
-      { id: 5, title: 'Fiszka 6', polish: 'rower', german: 'das Fahrrad', category: 'pojazdy' },
-      {
-        id: 6,
-        title: 'Fiszka 7',
-        polish: 'powiadomienie',
-        german: 'der Bescheid',
-        category: 'internet',
-      },
-      { id: 7, title: 'Fiszka 8', polish: 'jeść', german: 'essen', category: 'ogólne' },
-      { id: 8, title: 'Fiszka 9', polish: 'awantura', german: 'der Krach', category: 'emocje' },
-      { id: 9, title: 'Fiszka 10', polish: 'osoba', german: 'die Person', category: 'ludzie' },
-      {
-        id: 10,
-        title: 'Fiszka 11',
-        polish: 'termometr',
-        german: 'das Thermometer',
-        category: 'medycyna',
-      },
-      { id: 11, title: 'Fiszka 12', polish: 'cztery', german: 'vier', category: 'lidzby' },
-      { id: 12, title: 'Fiszka 13', polish: 'piątek', german: 'Freitag', category: 'czas' },
-    ],
+    flashsetID: this.props.id,
+    flashcards: [],
+    flashsets: {},
   };
 
+  getFlashcards = async () => {
+    const response = await fetch(`/api/flashsets/${this.state.flashsetID}`, setHeaders());
+    const body = await response.json();
+    this.setState({ flashsets: body });
+    // console.log(this.state.flashsets);
+  };
+
+  componentDidMount() {
+    //this.setState({ flashsetID: this.props.id });
+    console.log(this.state.flashsetID);
+    this.getFlashcards();
+  }
+
+  componentDidUpdate = async () => {
+    //this.setState({ flashsetID: this.props.id });
+    //console.log(this.state.flashsetID);
+    // this.setState({ flashsetID: this.props.id });
+    //await this.getFlashcards();
+  };
   handleOpen = (e) => {
     this.setState({ open: true });
-    this.setState({ curID: e.target.id });
-    console.log(e.target.id);
-    console.log(e.target.curTitle);
+    this.setState({
+      curID: e.target.id,
+      curPolish: e.target.value,
+      curGerman: e.target.name,
+    });
+    console.log(e.target);
+
+    //console.log(this.state.flashsetID);
+    //console.log(e.target.value);
   };
   handleClose = () => {
     this.setState({ open: false });
@@ -57,67 +55,55 @@ class CardsContent extends Component {
 
   handleOK = () => {};
 
-  handleChange = (value) => {
-    if (value <= 1) {
-      this.setState({
-        minValue: 0,
-        maxValue: 10,
-      });
-    } else {
-      this.setState({
-        minValue: this.state.maxValue,
-        maxValue: value * 10,
-      });
-    }
+  handleStar = () => {
+    this.setState({ temp: !this.state.temp });
   };
-
   render() {
     return (
-      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-        <div>
-          {this.state.data &&
-            this.state.data.length > 0 &&
-            this.state.data.slice(this.state.minValue, this.state.maxValue).map((val) => (
-              <Card
-                size="small"
-                title={val.title}
-                style={{ width: 230 }}
-                key={val.id}
-                //actions={[<EditOutlined key="edit" />]}
-              >
-                <h4>{val.polish}</h4>
-                <h4>{val.german}</h4>
-                <h6>{val.category}</h6>
-
-                <Button onClick={this.handleOpen} id={val.id}>
+      <>
+        <List
+          itemLayout="horizontal"
+          dataSource={this.state.flashsets.flashcards}
+          renderItem={(item) => (
+            <List.Item key={item._id}>
+              <div className="card">
+                <div className="card-polish">{item.polish}</div>
+                <div className="card-german">{item.german}</div>
+                <div className="card-category">
+                  KATEGORIA: <br />
+                  {item.category}
+                </div>
+                <Button
+                  className="card-icon"
+                  id={item._id}
+                  onClick={this.handleOpen}
+                  value={item.polish}
+                  name={item.german}
+                >
                   <EditOutlined key="edit" />
                 </Button>
-              </Card>
-            ))}
-        </div>
-        <div className="pagination">
-          <span style={{ padding: 5 }}></span>
-          <Pagination defaultCurrent={1} defaultPageSize={10} onChange={this.handleChange} total={20} />
-        </div>
+
+                <Button className="card-icon" onClick={this.handleStar} id={item.id}>
+                  {this.state.temp ? <StarOutlined key="add" /> : <StarFilled key="add" />}
+                </Button>
+              </div>
+            </List.Item>
+          )}
+        />
         <Modal
-          title="Edytuj"
+          title="Edycja fiszki"
           visible={this.state.open}
           onOk={this.handleOk}
           //confirmLoading={confirmLoading}
           onCancel={this.handleClose}
         >
-          <h2>Edycja fiszki numer {this.state.curID}</h2>
+          <h2>Edycja fiszki /zmienic czcionke/ ID: {this.state.curID}</h2>
           <h4>Polskie tłumaczenie:</h4>
-          <Input
-            style={{ marginTop: 10, marginBottom: 20 }}
-            placeholder={this.state.data[this.state.curID].polish}
-          ></Input>
+          <Input style={{ marginTop: 10, marginBottom: 20 }} placeholder={this.state.curPolish}></Input>
           <h4>Niemieckie tłumaczenie:</h4>
-          <Input style={{ marginBottom: 20 }} placeholder={this.state.data[this.state.curID].german}></Input>
-          <h4>Kategoria:</h4>
-          <Input style={{ marginBottom: 20 }} placeholder={this.state.data[this.state.curID].category}></Input>
+          <Input style={{ marginBottom: 20 }} placeholder={this.state.curGerman}></Input>
         </Modal>
-      </Row>
+      </>
     );
   }
 }
