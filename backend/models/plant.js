54b0d35785b0e2d1404c2e435a5c78511e06c4b2
Piotrek <piotrekste98@gmail.com>
2020-04-27 19:06:37 +0200

    quests endpoints

--- backend/models/plant.js
+++ backend/models/plant.js
@@ -44,6 +44,11 @@ const plantSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  flashsets: {
+    type: [ObjectId],
+    default: [],
+    ref: 'Flashsets',
+  },
   quests: {
     type: [ObjectId],
     default: [],
@@ -56,13 +61,9 @@ const plantSchema = new mongoose.Schema({
   },
 });
 
-const validatePlant = plant => {
+const validatePlant = (plant) => {
   const schema = Joi.object({
-    name: Joi.string()
-      .required()
-      .min(5)
-      .max(16)
-      .trim(),
+    name: Joi.string().required().min(5).max(16).trim(),
     species: Joi.valid('Bonsai', 'Cactus', 'Rose', 'Dandelion'),
     level: Joi.number().min(0),
     irrigation_points: Joi.number().min(0),
