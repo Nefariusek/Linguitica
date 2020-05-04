ea810c5328f2eb7f565aa30dff2fd97c6a2b9fce
Patrycja <Ciela>
2020-05-04 21:20:46 +0200

    learning style

---
+++ frontend/src/components/Learning/Menu.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import { MailOutlined, AppstoreOutlined } from '@ant-design/icons';
+
+class MenuLearning extends React.Component {
+  state = {
+    current: 'mail',
+  };
+  handleClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <Menu className="learningMenu" onSelect={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="mode" disabled>
+          Tryb nauki
+        </Menu.Item>
+        <Menu.Item key="cards">Fiszki</Menu.Item>
+
+        <Menu.Item key="write">Pisanie</Menu.Item>
+        <Menu.Item key="quiz"> Quiz</Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default MenuLearning;
