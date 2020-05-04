46b4aeb7b9df0b6060b833b45641078892ec7e34
Patrycja <Ciela>
2020-05-04 10:34:22 +0200

    guestpage changes

--- frontend/src/components/Guestpage/GuestpageContent.jsx
+++ frontend/src/components/Guestpage/GuestpageContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Button } from 'antd';
 import { Redirect } from 'react-router-dom';
+import studyTime from './../../images/studyTime.png';
 
 const { Content } = Layout;
 
@@ -25,50 +26,73 @@ class GuestpageContent extends React.Component {
     return (
       <Layout className="layout">
         <Content style={{ padding: '0' }}>
-          <div className="site-layout-content">
-            <div className="page">
-              <div className="front">
-                <div className="fiszki">
-                  <h1>Fiszki</h1>
-                </div>
+          <div className="startpage">
+            <div className="introduction">
+              <div className="intro-text">
+                <h1> Tutaj bedzie Mądre hasło1 </h1>
+                <p>A tu Madre hasło 2</p>
               </div>
-              <div className="back">
-                <div className="tekst">
-                  <h1>Ucz się słówek z każdej kategorii lub twórz własne zestawy fiszek </h1>
-                </div>
-              </div>
-            </div>
-
-            <div className="page">
-              <div className="front">
-                <div className="kalendarz">
-                  <h1>Kalendarz</h1>
-                </div>
-              </div>
-              <div className="back">
-                <div className="tekst">
-                  <h1>Stawiaj sobie nowe zadania do wykonania, my przypmonimy Ci o ich wykonaniu!</h1>
-                </div>
+              <div buttons>
+                <Button className="btn-start" onClick={this.handleCLick}>
+                  DOŁĄCZ DO NAS
+                </Button>
+                <Button className="btn-more">POZNAJ NAS</Button>
               </div>
             </div>
-
-            <div className="page">
-              <div className="front">
-                <div className="testy">
-                  <h1>Testy</h1>
-                </div>
-              </div>
-              <div className="back">
-                <div className="tekst">
-                  <h1> Testuj zdobytą wiedzę, zdobywaj cenne puknty i zadbaj o swoją roślinkę</h1>
-                </div>
-              </div>
+            <div className="start-cover">
+              <img src={studyTime} />
             </div>
           </div>
-          <div className="homepageButton">
-            <Button type="primary" className="joinButton" onClick={this.handleCLick}>
-              DOŁĄCZ DO NAS
-            </Button>
+
+          <div className="site-layout-content">
+            <div className="blocks-title">
+              <h1>Sprawdź, co dla Ciebie przygotowaliśmy!</h1>
+            </div>
+            <div className="blocks">
+              <div className="page">
+                <div className="front">
+                  <div className="fiszki">
+                    <h1>Fiszki</h1>
+                  </div>
+                </div>
+                <div className="back">
+                  <div className="tekst">
+                    <h1>Ucz się słówek z każdej kategorii lub twórz własne zestawy fiszek </h1>
+                  </div>
+                </div>
+              </div>
+
+              <div className="page">
+                <div className="front">
+                  <div className="testy">
+                    <h1>Testy</h1>
+                  </div>
+                </div>
+                <div className="back">
+                  <div className="tekst">
+                    <h1> Testuj zdobytą wiedzę, zdobywaj cenne puknty i zadbaj o swoją roślinkę</h1>
+                  </div>
+                </div>
+              </div>
+
+              <div className="page">
+                <div className="front">
+                  <div className="kalendarz">
+                    <h1>Kalendarz</h1>
+                  </div>
+                </div>
+                <div className="back">
+                  <div className="tekst">
+                    <h1>Stawiaj sobie nowe zadania do wykonania, my przypmonimy Ci o ich wykonaniu!</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="homepageButton">
+              <Button className="btn-join" onClick={this.handleCLick}>
+                DOŁĄCZ DO NAS
+              </Button>
+            </div>
           </div>
         </Content>
       </Layout>
