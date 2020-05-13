9e23cc3d1d616c7bb3a29f8337e61f5f31c3406c
Piotrek <piotrekste98@gmail.com>
2020-05-13 15:57:25 +0200

    second version of quests

--- frontend/src/components/Quests/QuestsContent.jsx
+++ frontend/src/components/Quests/QuestsContent.jsx
@@ -13,15 +13,18 @@ class QuestsContent extends Component {
     plantID: '5e85bd0c0fc921686c98dd1d', //tymczasowo dopoki nie naprawimy store
     questsID: [],
     quests: [],
+    allQuests: [],
     isLoaded: false,
     deadline: [],
-    current: 'all',
+    current: 'your',
     visible: false,
     curID: '0',
+    secondCurID: '0',
     priority: 0,
     description: '',
     date: '',
     goal: '',
+    addedQuestID: '0',
   };
 
   static contextType = Store;
@@ -31,9 +34,11 @@ class QuestsContent extends Component {
     let count = this.state.questsID.length;
     if (count > 0) {
       await this.getQuests(count);
+      await this.getAllQuests();
       await this.setState({ isLoaded: true });
     }
   };
+
   getQuestsID = async () => {
     await axios({
       // url: `/api/plants/${this.context.userProfile.plant_id}`,
@@ -69,28 +74,84 @@ class QuestsContent extends Component {
     }
   };
 
+  getAllQuests = async () => {
+    const response = await fetch('/api/quests', setHeaders());
+    const body = await response.json();
+    let filteredBody = body;
+
+    filteredBody = body.filter((quests) => {
+      return quests.copy !== true;
+    });
+
+    await this.setState({ allQuests: filteredBody });
+    console.log(this.state.allQuests);
+  };
   postQuest = async () => {
     await axios({
       url: 'api/quests/',
       method: 'post',
       data: {
         status: 'failed',
-        description: 'bardzo wazny quest',
-        goal: 'goal goal goal',
-        flashset_id: ['5e85bd0c0fc921686c98dd0a', '5e85bd0c0fc921686c98dd0b'],
+        duration: this.state.allQuests[this.state.secondCurID].duration,
+        description: this.state.allQuests[this.state.secondCurID].description,
+        is_requrring: this.state.allQuests[this.state.secondCurID].is_requrring,
+        flashset_id: this.state.allQuests[this.state.secondCurID].flashset_id,
+        goal: this.state.allQuests[this.state.secondCurID].goal,
+        copy: true,
       },
       headers: setHeaders(),
     }).then(
       (res) => {
         if (res.status === 200) {
-          console.log('Dodano quest');
-          message.success('Quest created', 3);
+          console.log(res.data._id);
+          this.setState({ addedQuestID: res.data._id });
+          console.log(this.state.addedQuestID);
+          message.success('Quest dodany', 3);
         }
       },
       (err) => {
         console.log(err);
       },
     );
+  };
+
+  putQuest = async () => {
+    await axios({
+      url: `/api/plants/${this.state.plantID}/quests`,
+      method: 'put',
+      data: {
+        quests: {
+          _id: this.state.addedQuestID,
+        },
+      },
+      headers: setHeaders(),
+    }).then(
+      (res) => {
+        if (res.status === 200) {
+          console.log('quest dodany do planta');
+        }
+      },
+      (err) => {
+        console.log(err);
+      },
+    );
+  };
+  addQuest = async (e) => {
+    await this.setState({
+      secondCurID: e.target.value,
+      questsID: [],
+      quests: [],
+    });
+    await this.postQuest();
+    await this.putQuest();
+    // await this.setState({ isLoaded: false });
+
+    await this.getQuestsID();
+    let count = this.state.questsID.length;
+    if (count > 0) {
+      await this.getQuests(count);
+      await this.setState({ isLoaded: true });
+    }
   };
   handleChange = async (e) => {
     const { value, name } = e.target;
@@ -111,20 +172,21 @@ class QuestsContent extends Component {
       visible: true,
     });
   };
-
+  showModalAll = async (e) => {
+    await this.setState({
+      secondCurID: e.target.value,
+    });
+    await this.setState({
+      visible: true,
+    });
+  };
   handleOk = (e) => {
     console.log(e);
     this.setState({
       visible: false,
     });
   };
-  onFinish = async () => {
-    await console.log(this.state.name);
-    await console.log(this.state.description);
-    await console.log(this.state.priority);
-    await console.log(this.state.date);
-    await console.log(this.state.goal);
-  };
+
   onChangeDate = (value, dateString) => {
     this.setState({ date: dateString });
   };
@@ -136,6 +198,63 @@ class QuestsContent extends Component {
       visible: false,
     });
   };
+
+  cancelButton = async (e) => {
+    await this.setState({
+      curID: e.target.value,
+    });
+    var moment = require('moment'); // require
+    var mom = moment().format();
+    await axios({
+      url: `/api/quests/${this.state.quests[this.state.curID]._id}/status`,
+      method: 'put',
+      headers: setHeaders(),
+      data: {
+        status: 'failed',
+        finish_date: mom,
+      },
+    }).then(
+      (res) => {},
+      (error) => {
+        console.log(error);
+      },
+    );
+    await this.reload();
+  };
+
+  startButton = async (e) => {
+    await this.setState({
+      curID: e.target.value,
+    });
+    var moment = require('moment'); // require
+    var mom = moment().add(this.state.quests[this.state.curID].duration, 'day').format();
+    await axios({
+      url: `/api/quests/${this.state.quests[this.state.curID]._id}/status`,
+      method: 'put',
+      headers: setHeaders(),
+      data: {
+        status: 'in_progress',
+        finish_date: mom,
+      },
+    }).then(
+      (res) => {},
+      (error) => {
+        console.log(error);
+      },
+    );
+    await this.reload();
+  };
+  reload = async () => {
+    await this.setState({
+      quests: [],
+    });
+    //await this.getQuestsID();
+    let count = this.state.questsID.length;
+    // if (count > 0) {
+    await this.getQuests(count);
+    await this.setState({ isLoaded: true });
+    // }
+  };
   render() {
     const layout = {
       labelCol: { span: 5 },
@@ -146,14 +265,11 @@ class QuestsContent extends Component {
       <>
         <div className="nav-quests">
           <Menu onClick={this.handleMenuClick} selectedKeys={[this.state.current]} mode="horizontal">
+            <Menu.Item className="nav-item-quests" key="your">
+              Twoje zadania
+            </Menu.Item>
             <Menu.Item className="nav-item-quests" key="all">
               Wszystkie zadania
-            </Menu.Item>
-            <Menu.Item className="nav-item-quests" key="finished">
-              Ukończone
-            </Menu.Item>
-            <Menu.Item className="nav-item-quests" key="current">
-              Trwające
             </Menu.Item>
 
             <Menu.Item className="nav-item-quests" key="new">
@@ -164,7 +280,7 @@ class QuestsContent extends Component {
         </div>
 
         <div>
-          {(this.state.current === 'all' && (
+          {(this.state.current === 'your' && (
             <div className="container-all">
               <div>
                 {this.state.isLoaded ? (
@@ -173,17 +289,47 @@ class QuestsContent extends Component {
                       <h2>opis</h2>
                       <div className="quest-description"> {val.description}</div>
                       <div className="quest-attributes">
-                        Duration: {val.duration} Priority: {val.priority} Goal: {val.goal}
+                        Długość: {val.duration}dni Priorytet: {val.priority} Cel: {val.goal}
                       </div>
                       <div className="quest-info-button">
                         <Button value={key} style={{ height: '7vh', borderRadius: '1000px' }} onClick={this.showModal}>
                           INFO
                         </Button>
                       </div>
-                      <div className="quest-countdown">
-                        {' '}
-                        <Countdown title="Pozostały czas:" value={val.finish_date} format="D dni HH:mm:ss" />{' '}
-                      </div>
+                      {(val.status === 'in_progress' && (
+                        <div>
+                          <div className="quest-info-button">
+                            <Button
+                              value={key}
+                              style={{ height: '7vh', borderRadius: '1000px' }}
+                              onClick={this.cancelButton}
+                            >
+                              ANULUJ
+                            </Button>
+                          </div>
+                          <div className="quest-countdown">
+                            {' '}
+                            <Countdown title="Pozostały czas:" value={val.finish_date} format="D dni HH:mm:ss" />{' '}
+                          </div>
+                        </div>
+                      )) ||
+                        (val.status === 'failed' && (
+                          <>
+                            <div className="quest-info-button">
+                              <Button
+                                value={key}
+                                style={{ height: '7vh', borderRadius: '1000px' }}
+                                onClick={this.startButton}
+                              >
+                                ROZPOCZNIJ
+                              </Button>
+                            </div>
+                            <div className="quest-countdown">
+                              <Countdown title="Pozostały czas:" value={val.finish_date} format="D dni HH:mm:ss" />
+                            </div>
+                          </>
+                        )) || <div>Ukonczyles questa</div>}
+
                       <Modal
                         key={val.key}
                         title="Szczegółowe informacje"
@@ -224,8 +370,66 @@ class QuestsContent extends Component {
               </div>
             </div>
           )) ||
-            (this.state.current === 'finished' && <div className="container-all">zakonczone</div>) ||
-            (this.state.current === 'current' && <div className="container-all">Trwające</div>) ||
+            (this.state.current === 'all' && (
+              <div className="container-all">
+                <div>
+                  {this.state.isLoaded ? (
+                    this.state.allQuests.map((val, key) => (
+                      <div className="quest-all" key={key}>
+                        <h2>opis</h2>
+                        <div className="quest-description"> {val.description}</div>
+                        <div className="quest-attributes">
+                          Czas trwania: {val.duration}dni Priorytet: {val.priority} Cel: {val.goal}
+                        </div>
+                        <div className="quest-info-button">
+                          <Button
+                            value={key}
+                            style={{ height: '7vh', borderRadius: '1000px' }}
+                            onClick={this.showModalAll}
+                          >
+                            INFORMACJE
+                          </Button>
+                          <Button value={key} style={{ height: '7vh', borderRadius: '1000px' }} onClick={this.addQuest}>
+                            DODAJ DO MOJEJ LISTY
+                          </Button>
+                        </div>
+
+                        <Modal
+                          key={val.key}
+                          title="Szczegółowe informacje"
+                          visible={this.state.visible}
+                          onOk={this.handleCancel}
+                          onCancel={this.handleCancel}
+                        >
+                          <p>
+                            <h3>Opis:</h3>
+                            {this.state.allQuests[this.state.secondCurID].description}
+                          </p>
+                          <p>
+                            <h3>Cel:</h3>
+                            {this.state.allQuests[this.state.secondCurID].goal}
+                          </p>
+                          <p>
+                            <h3>Priorytet:</h3>
+                            {this.state.allQuests[this.state.secondCurID].priority}
+                          </p>
+                          <p>
+                            <h3>Kategoria:</h3>
+                            {this.state.allQuests[this.state.secondCurID].category}
+                          </p>
+                          <p>
+                            <h3>Czas trwania:</h3>
+                            {this.state.allQuests[this.state.secondCurID].duration}dni
+                          </p>
+                        </Modal>
+                      </div>
+                    ))
+                  ) : (
+                    <LoadingOutlined />
+                  )}
+                </div>
+              </div>
+            )) ||
             (this.state.current === 'new' && (
               <div className="container-all">
                 <div className="quest-add">
