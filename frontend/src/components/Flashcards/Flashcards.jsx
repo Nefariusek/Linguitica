30e6b81cbe83f8429237a5d2fd9be967d7bc9787
Piotrek <piotrekste98@gmail.com>
2020-05-16 23:31:32 +0200

    new style of flashcards

--- frontend/src/components/Flashcards/Flashcards.jsx
+++ frontend/src/components/Flashcards/Flashcards.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { List, Button } from 'antd';
-import { PlusCircleOutlined, PlusCircleFilled } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { Button, Table } from 'antd';
 import setHeaders from '../../utils/setHeaders';
 class Cards extends Component {
   state = {
@@ -10,6 +10,10 @@ class Cards extends Component {
     level: this.props.level,
     selected: [],
     names: [],
+    selectedRowKeys: [],
+    selectedFlashcards: [],
+    data: [],
+    loaded: false,
   };
 
   getFlashcards = async () => {
@@ -25,6 +29,18 @@ class Cards extends Component {
 
     this.setState({ flashcards: filteredBody });
     console.log(this.state.flashcards);
+
+    for (let i = 0; i < this.state.flashcards.length; i++) {
+      await this.state.data.push({
+        key: i,
+        polish: this.state.flashcards[i].polish,
+        german: this.state.flashcards[i].german,
+        category: this.state.flashcards[i].category,
+        level: this.state.flashcards[i].level,
+      });
+    }
+    this.setState({ loaded: true });
+    console.log('data', this.state.data);
   };
 
   handleSelect = async (e) => {
@@ -71,11 +87,78 @@ class Cards extends Component {
       console.log('szukany poziom:', this.state.level);
     }
   };
+  onSelectChange = async (selectedRowKeys) => {
+    await this.setState({ selectedFlashcards: [] });
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    await this.setState({ selectedRowKeys });
 
+    for (let i = 0; i < selectedRowKeys.length; i++) {
+      this.state.selectedFlashcards.push(this.state.flashcards[selectedRowKeys[i]]);
+    }
+    console.log('wybrane fiszki: ', this.state.selectedFlashcards);
+  };
   render() {
+    const columns = [
+      {
+        title: 'Polski',
+        dataIndex: 'polish',
+      },
+      {
+        title: 'Niemiecki',
+        dataIndex: 'german',
+      },
+      {
+        title: 'Kategoria',
+        dataIndex: 'category',
+      },
+      {
+        title: 'Poziom',
+        dataIndex: 'level',
+      },
+    ];
+    const { loading, selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
     return (
       <div>
-        <List
+        <div className="flashcards-container">
+          <div style={{ marginBottom: 16 }}>
+            <Link
+              to={{
+                pathname: '/learning',
+                flashcards: this.state.selectedFlashcards,
+              }}
+            >
+              <Button
+                style={{ width: '10vw' }}
+                type="primary"
+                onClick={this.handleStart}
+                disabled={!hasSelected}
+                loading={loading}
+              >
+                Nauka
+              </Button>
+            </Link>
+            <span style={{ marginLeft: 8 }}>{hasSelected ? `Wybrano ${selectedRowKeys.length}` : ''}</span>
+          </div>
+
+          {this.state.loaded ? (
+            <Table rowSelection={rowSelection} columns={columns} dataSource={this.state.data}></Table>
+          ) : (
+            <h1></h1>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cards;
+
+/*<List
           itemLayout="horizontal"
           dataSource={this.state.flashcards}
           renderItem={(item, index) => (
@@ -105,10 +188,4 @@ class Cards extends Component {
               </div>
             </List.Item>
           )}
-        />
-      </div>
-    );
-  }
-}
-
-export default Cards;
+        /> */
