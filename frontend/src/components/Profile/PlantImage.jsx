d2c66060e5e570af7b98fbc8610fd1389f041408
Szymon <szymonpakula98@gmail.com>
2020-05-16 02:21:51 +0200

    very basic version of plant profile

---
+++ frontend/src/components/Profile/PlantImage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlantImage extends Component {
+  render() {
+    return (
+      <div>
+        <div>Obrazek</div>
+      </div>
+    );
+  }
+}
+
+export default PlantImage;
