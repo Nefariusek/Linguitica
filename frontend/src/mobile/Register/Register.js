d10800d0997be6ec1253880e9055350394c0933d
Piotrek <piotrekste98@gmail.com>
2020-05-28 23:01:11 +0200

    first version of register

---
+++ frontend/src/mobile/Register/Register.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import { Container, Header, Title, Left, Icon, Right, Button, Body, Content, Text, Item, Input } from 'native-base';
+import Logo from '../../images/logo_mobile.png';
+export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header style={{ backgroundColor: '#1890ff' }}>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.openDrawer()}>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Zarejestruj się</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <ImageBackground
+            source={Logo}
+            style={{
+              height: 70,
+              width: 300,
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingBottom: 0,
+              marginTop: 70,
+              marginBottom: 70,
+            }}
+          />
+          <Item
+            rounded
+            style={{ marginTop: 10, height: 60, width: 350, marginLeft: 'auto', marginRight: 'auto' }}
+            autoCompleteType="login"
+          >
+            <Input placeholder="LOGIN" />
+          </Item>
+          <Item
+            rounded
+            style={{ marginTop: 10, height: 60, width: 350, marginLeft: 'auto', marginRight: 'auto' }}
+            autoCompleteType="email"
+          >
+            <Input placeholder="EMAIL" />
+          </Item>
+          <Item
+            rounded
+            style={{ marginTop: 10, height: 60, width: 350, marginLeft: 'auto', marginRight: 'auto' }}
+            autoCompleteType="password"
+          >
+            <Input placeholder="HASLO" />
+          </Item>
+          <Item
+            rounded
+            style={{ marginTop: 10, height: 60, width: 350, marginLeft: 'auto', marginRight: 'auto' }}
+            autoCompleteType="password"
+          >
+            <Input placeholder="POWTÓRZ HASLO" />
+          </Item>
+
+          <Button
+            full
+            rounded
+            style={{
+              marginTop: 50,
+              backgroundColor: '#1890ff',
+              height: 60,
+              width: 250,
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            onPress={() => this.props.navigation.navigate('Flashcards')}
+          >
+            <Text>Zarejestruj się</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
